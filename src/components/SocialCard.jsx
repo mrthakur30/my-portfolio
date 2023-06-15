2746360707b37e78682385b3a97fb9ac43c2475e
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2023-06-15 16:17:40 +0530

    dropdown responsive-1

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 function SocialCard({ name, link, imgURL }) {
   return (
     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
-      <Link rel="noreferrer" target="_blank" href={link}>
+      <a rel="noreferrer" target="_blank" href={link}>
         <div className="flex flex-row bg-slate-50 border-2 gap-2  border-gray-400 hover:border-white  rounded-md p-1  transition-all duration-200  hover:bg-stone-100 items-center ">
           <img className="inline-block h-14   rounded-md" src={imgURL} />
           <span className="text-lg font-bold">{name}</span>
         </div>
-      </Link>
+      </a>
     </motion.div>
   );
 }
